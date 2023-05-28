4666f404b5173b809f221cce7a2e09d0faabcc98
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:06:43 +0400

    added a function that compares the hash in the task with the hash of the card number, accepts arguments: hash in the task, bank card number. Returns the result of comparing two hashes

--- card.py
+++ card.py
@@ -3,3 +3,19 @@ from tqdm import tqdm
 import logging
 import multiprocessing as mp
 
+
+def check_hash(hash: str, card_number: str) -> bool:
+    """Compares hash in the task with the hash of the card number
+
+    Args:
+        hash (str): hash in the task
+        card_number (str): number of the bank card
+
+    Returns:
+        bool: result of comparison of two hashes
+    """
+    logging.info("Checking the hash")
+    card_hash = hashlib.sha256(card_number.encode()).hexdigest()
+    if hash == card_hash:
+        return int(card_number)
+    return 0
