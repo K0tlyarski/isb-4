eb81beae36d32e6c0ec4f02f8f4b49f5b0cdf0e2
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:12:49 +0400

    Added a function that selects the card number and accepts arguments: hash, a list of bins, the last 4 card numbers, the number of processor cores. Returns the card number or 0 in case of failure

--- card.py
+++ card.py
@@ -19,3 +19,29 @@ def check_hash(hash: str, card_number: str) -> bool:
     if hash == card_hash:
         return int(card_number)
     return 0
+
+
+def get_card_number(hash: str, bins: list, last_numbs: str, core_number: int = mp.cpu_count()) -> int:
+    """Selects the card number
+
+    Args:
+        hash (str): hash in the task
+        bins (list): bins of the cards
+        last_numbs (str): last 4 numbers of the card
+        core_number (int): number of processor cores
+
+    Returns:
+        int: card number or 0 if failed
+    """
+    args = []
+    for i in range(100000,1000000):
+        for j in bins:
+            args.append((hash, f"{j}{i}{last_numbs}"))
+    with mp.Pool(processes=core_number) as p:
+        for result in p.starmap(check_hash, tqdm(args, ncols=120, colour="green")):
+            if result:
+                p.terminate()
+                logging.info("Card number got successfully")
+                return result
+    logging.info("Card number not got")
+    return 0
