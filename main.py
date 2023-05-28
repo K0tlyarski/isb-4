4dff06853382302fb5c4b1f429e7fae8c2fba77d
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:42:40 +0400

    adding parser arguments

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import time
 import load_write
 from card import get_card_number, luhn_algorithm
 import stats
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        filename="prog_logs.log", filemode="w")
+    settings = load_write.load_settings("settings.json")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-card", "--get_card_numb", type=int,
+                        help="Gets card number, input the number of processors")
+    parser.add_argument("-luhn_algorithm", "--luhn_algorithm",
+                        help="Checking the card number using the Luhn algorithm")
+    parser.add_argument("-stat", "--statistics",
+                        help="Gets stats about executing time with different number of processors")
+    args = parser.parse_args()
