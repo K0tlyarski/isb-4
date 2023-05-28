c016bf87239edc710078cb79a87c471e00fb9a83
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:20:42 +0400

    added a function that reads the file and takes the file name argument, and returns the data in the file

--- load_write.py
+++ load_write.py
@@ -1,2 +1,21 @@
 import logging
 import json
+
+
+def read_file(file_name: str) -> str:
+    """Reads the file
+
+    Args:
+        file_name(str): name of the file
+
+    Returns:
+        str: data in the file
+    """
+    try:
+        with open(file_name, "r") as f:
+            data = f.read()
+        logging.info("Read the data successfully")
+    except OSError as err:
+        logging.info("Read the data is failed")
+        raise err
+    return data
