dacef2c622d3d31cf856f52b9dbd373c33afd7df
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:30:25 +0400

    Added a function that draws and saves a graph based on statistics. Accepts arguments: data for the graph and a path to save

--- stats.py
+++ stats.py
@@ -1,3 +1,21 @@
 import logging
 import csv
 import matplotlib.pyplot as plt
+
+
+def create_stats(statistics: dict, file_name: str) -> None:
+    """Draws and saves the plot based on the statistic
+
+    Args:
+        statistic(dict): data for the plot
+        file_name(str): path for saving
+    """
+    fig = plt.figure(figsize=(30, 5))
+    plt.ylabel("Time")
+    plt.xlabel("Processes")
+    plt.title("Stats")
+    x = statistics.keys()
+    y = statistics.values()
+    plt.bar(x, y, color="blue", width=0.5)
+    plt.savefig(file_name)
+    logging.info("Create and save statistics")
