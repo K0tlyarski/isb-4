40dc5ce07a107d980a49e82799dd1dae45020169
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:34:04 +0400

    fixed a tab error

--- stats.py
+++ stats.py
@@ -21,19 +21,19 @@ def create_stats(statistics: dict, file_name: str) -> None:
     logging.info("Create and save statistics")
 
 
-    def write_stats(processes: int, time: float, file_name: str) -> None:
-        """Writes statistic to the file
+def write_stats(processes: int, time: float, file_name: str) -> None:
+    """Writes statistic to the file
 
-        Args:
-            processes(int): number of processes
-            time(float): executing time
-            file_name(str): name of the file
-        """
-        try:
-            with open(file_name, 'a', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([processes, time])
-            logging.info("Stats successfully written")
-        except OSError as err:
-            logging.info("Stats writing is failed")
-            raise err
+    Args:
+        processes(int): number of processes
+        time(float): executing time
+        file_name(str): name of the file
+    """
+    try:
+        with open(file_name, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([processes, time])
+        logging.info("Stats successfully written")
+    except OSError as err:
+        logging.info("Stats writing is failed")
+        raise err
