7d41879440fc357e6d3bd1d91c102a8e7db80923
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:22:42 +0400

    Added a function that writes data to a file and accepts arguments: data to be written and the name of the file to be written tole

--- load_write.py
+++ load_write.py
@@ -19,3 +19,19 @@ def read_file(file_name: str) -> str:
         logging.info("Read the data is failed")
         raise err
     return data
+
+
+def write_file(data: str, file_name: str) -> None:
+    """Writes the data in the file
+
+    Args:
+        data(str): data for writting
+        file_name(str): name of the file where to write
+    """
+    try:
+        with open(file_name, "w") as f:
+            f.write(data)
+        logging.info("Write the data successfully")
+    except OSError as err:
+        logging.info("Write the data is failed")
+        raise err
