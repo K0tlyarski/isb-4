f48ed9a6e79ace89231fc22377c6fa04a1a41ad7
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-28 19:31:54 +0400

    Added a function that writes statistics to a file. Accepts arguments: number of processes, execution time, and file name

--- stats.py
+++ stats.py
@@ -19,3 +19,21 @@ def create_stats(statistics: dict, file_name: str) -> None:
     plt.bar(x, y, color="blue", width=0.5)
     plt.savefig(file_name)
     logging.info("Create and save statistics")
+
+
+    def write_stats(processes: int, time: float, file_name: str) -> None:
+        """Writes statistic to the file
+
+        Args:
+            processes(int): number of processes
+            time(float): executing time
+            file_name(str): name of the file
+        """
+        try:
+            with open(file_name, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([processes, time])
+            logging.info("Stats successfully written")
+        except OSError as err:
+            logging.info("Stats writing is failed")
+            raise err
